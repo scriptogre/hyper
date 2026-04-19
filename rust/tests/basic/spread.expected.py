f7b950b9dde554dd425b3c9f9998729ee3b13bc5
Christian Tanul <git@christiantanul.com>
2026-04-19 13:22:04 +0300

    Add HTML element spread test with {**attrs} syntax

--- rust/tests/basic/spread.expected.py
+++ rust/tests/basic/spread.expected.py
@@ -1,6 +1,11 @@
-from hyper import html, render_attr
+from hyper import html, spread_attrs
 
 
 @html
-def Spread(*, attrs: dict = {"href": "https://example.com", "target": "_blank"}):
-    yield f"""<a{render_attr("attrs", attrs)}>Link</a>"""
+def Spread(*, attrs: dict = {"href": "https://example.com", "target": "_blank"}, extra: dict):
+
+    # Spread on HTML element
+    yield f"""<a{spread_attrs(attrs)}>External link</a>"""
+
+    # Spread with other attributes
+    yield f"""<div id="main"{spread_attrs(extra)} class="container">Content</div>"""
